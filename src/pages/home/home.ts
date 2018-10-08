fd83fa4a0cb0b59c76fe90393fc8b45be87ec0cb
predator_72005@ukr.net <puny.flash@gmail.com>
2018-10-08 16:01:17 +0300

    Load data from json file, platforms settings

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get<any[]>('assets/data-rus.min.json').subscribe(data => {
+      var themes = document.getElementById("themes");
+      data.forEach(function(element, index) {
+        var li = document.createElement("li");
+        li.innerHTML = "<a href=\"#"+index+"\">"+ element.theme +"</a>";
+        themes.appendChild(li);
+      });
+    });
   }
 
 }
